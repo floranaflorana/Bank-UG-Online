6d544d053aed6082a27f54c3514584a2a1e6ae87
floranaflorana <105246868+floranaflorana@users.noreply.github.com>
2023-04-03 14:53:13 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 # Here we're importing Django's function path and all of our views from the blog application
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.post_list, name = 'post_list'),
 ]
